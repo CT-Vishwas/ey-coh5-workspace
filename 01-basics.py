33824672efd61efd9a3fb73d7de774db205a0459
Vishwas K Singh <vishwas@cloudthat.com>
2026-06-01 17:34:49 +0530

    Signed-off-by: Vishwas K Singh <vishwas@cloudthat.com>

---
+++ 01-basics.py
@@ -0,0 +1,12 @@
+
+# This is a comment
+# name = input("Enter your name: ")
+# num1 = int(input("Enter first Number: ")) # Here we take first number
+# num2 = int(input("Enter second Number: "))
+# print(f"Welcome {name}, the sum of numbers entered is: {num1+num2}")
+# print("Welcome", name," the sum of numbers entered is: ",num1+num2)
+# print("Welcome "+ name +" the sum of numbers entered is: "+ str(num1+num2))
+
+print("Hello", end="**")
+print("Vishwas", end="*")
+print("Singh")
